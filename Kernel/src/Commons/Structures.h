afac165367a9a8a4c04e4615f695d65b55a74f44
Jonatan Mild <jbmild@gmail.com>
2017-04-01 18:02:05 -0300

    Creacion de proyectos

---
+++ Kernel/src/Commons/Structures.h
@@ -0,0 +1,13 @@
+/*
+ * Structures.h
+ *
+ *  Created on: 1/4/2017
+ *      Author: utnso
+ */
+
+#ifndef COMMONS_STRUCTURES_H_
+#define COMMONS_STRUCTURES_H_
+
+
+
+#endif /* COMMONS_STRUCTURES_H_ */
