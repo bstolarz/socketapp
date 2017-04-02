7fc2bcf4107dafc57049e19031686e94823642b8
Francisco Patalagoity <fpatalagoity@hotmailcom>
2017-04-02 16:09:30 -0300

    leo archivo de configuracion

--- Console/src/Console.c
+++ Console/src/Console.c
@@ -10,9 +10,28 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <commons/config.h>
+#include "commons/structures.h"
 
-int main(void) {
+void leerArchivoConfiguracion(char* path){
+
+	t_config* config=config_create(path);
+	t_console* console=malloc(sizeof(t_console));
+	console->ip_kernel=config_get_string_value(config,"IP_KERNEL");
+	console->puerto_kernel=config_get_int_value(config,"PUERTO_KERNEL");
+	printf("La ip del kernel es: %s\n",console->ip_kernel);
+	printf("El puerto del kernel es: %d\n",console->puerto_kernel);
+}
+
+int main(int arg, char* argv[]) {
 	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
 	puts("It is working now");
+	if(arg!=2){
+		printf("Te falta poner el path! %d\n", arg);
+		return 1;
+	}
+	leerArchivoConfiguracion(argv[1]);
+	//leerArchivoConfiguracion("/home/utnso/git/tp-2017-1c-SocketApp/console");
+
 	return EXIT_SUCCESS;
 }
