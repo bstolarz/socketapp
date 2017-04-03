0012a2d0510d04813e6f6f621dd342a7f5a4e1b7
Jonatan Mild <jbmild@gmail.com>
2017-04-02 23:19:34 -0300

    kernel

---
+++ Kernel/src/commons/definitions.h
@@ -0,0 +1,16 @@
+/*
+ * definitions.h
+ *
+ *  Created on: 2/4/2017
+ *      Author: utnso
+ */
+
+#include <commons/collections/list.h>
+
+#ifndef COMMONS_DEFINITIONS_H_
+#define COMMONS_DEFINITIONS_H_
+
+pthread_t selectThread;
+t_list* clientes;
+
+#endif /* COMMONS_DEFINITIONS_H_ */
---
+++ Kernel/src/threads/select.h
@@ -0,0 +1,13 @@
+/*
+ * select.h
+ *
+ *  Created on: 2/4/2017
+ *      Author: utnso
+ */
+
+#ifndef THREADS_SELECT_H_
+#define THREADS_SELECT_H_
+
+void* selectThreadLauncher(void* arg);
+
+#endif /* THREADS_SELECT_H_ */
