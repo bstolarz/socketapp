3250334d32a767d5f0695df53978766680a54a6d
Francisco Patalagoity <fpatalagoity@hotmailcom>
2017-04-03 00:11:14 -0300

    lectura casi completa del archivo de configuracion del kernel

--- Kernel/src/Kernel.c
+++ Kernel/src/Kernel.c
@@ -11,17 +11,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <commons/collections/list.h>
+#include <commons/config.h>
 #include <signal.h>
 #include <pthread.h>
 #include <errno.h>
-#include "commons/definitions.h"
-#include "threads/select.h"
+#include "commons/structures.h"
+//#include "commons/definitions.h"
+//#include "threads/select.h"
 
-int main(void) {
+void leerConfiguracionDeKernel(char* path){
+	t_config* config=config_create(path);
+	t_console* console=malloc(sizeof(t_console));
+	console->algoritmo=config_get_string_value(config,"ALGORITMO");
+	printf("Algoritmo: %s\n", console->algoritmo);
+	console->grado_multiprog=config_get_int_value(config,"GRADO_MULTIPROG");
+	printf("Grado multip: %d\n",console->grado_multiprog);
+	console->ip_fs=config_get_string_value(config,"IP_FS");
+	printf("IP FS: %s\n", console->ip_fs);
+	console->ip_memoria=config_get_string_value(config,"IP_MEMORIA");
+	printf("IP MEMORIA: %s\n", console->ip_memoria);
+	console->puerto_cpu=config_get_int_value(config,"PUERTO_CPU");
+	printf("PUERTO CPU: %d\n", console->puerto_cpu);
+	console->puerto_fs=config_get_int_value(config,"PUERTO_FS");
+	printf("PUERTO FS: %d\n", console->puerto_fs);
+	console->puerto_memoria=config_get_int_value(config,"PUERTO_MEMORIA");
+	printf("PUERTO MEMORIA: %d\n", console->puerto_memoria);
+	console->puerto_prog=config_get_int_value(config,"PUERTO_PROG");
+	printf("PUERTO PROG: %d\n", console->puerto_prog);
+	console->quantum=config_get_int_value(config,"QUANTUM");
+	printf("QUANTUM: %d\n", console->quantum);
+	console->quantum_sleep=config_get_int_value(config,"QUANTUM_SLEEP");
+	printf("QUANTUM SLEEP: %d\n", console->quantum_sleep);
+	console->stack_size=config_get_int_value(config,"STACK_SIZE");
+	printf("STACK SIZE: %d\n", console->stack_size);
+	char** vec =config_get_array_value(config,"SEM_IDS");
+	int i;
+	for (i=0;i<string_length(*vec)-1;i++){
+		printf("SEM %d ID: %s\n",i, vec[i]);
+	}
 
-	clientes = list_create();
-	pthread_create(&selectThread,NULL,selectThreadLauncher, NULL);
-	pthread_join(selectThread, NULL);
+}
+int main(int args, char* argv[]) {
+
+	//clientes = list_create();
+	//pthread_create(&selectThread,NULL,selectThreadLauncher, NULL);
+	//pthread_join(selectThread, NULL);
+	leerConfiguracionDeKernel(argv[1]);
 
 
 	return EXIT_SUCCESS;
--- Kernel/src/commons/structures.h
+++ Kernel/src/commons/structures.h
@@ -8,6 +8,21 @@
 #ifndef COMMONS_STRUCTURES_H_
 #define COMMONS_STRUCTURES_H_
 
-
+typedef struct{
+	int puerto_prog;
+	int puerto_cpu;
+	char* ip_memoria;
+	int puerto_memoria;
+	char* ip_fs;
+	int puerto_fs;
+	int quantum;
+	int quantum_sleep;
+	char* algoritmo;
+	int grado_multiprog;
+	t_list* sem_ids;
+	t_list* sem_init;
+	t_list* shared_vars;
+	int stack_size;
+}t_console;
 
 #endif /* COMMONS_STRUCTURES_H_ */
