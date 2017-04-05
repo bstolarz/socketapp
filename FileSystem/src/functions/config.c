a18a222fe2bdff85e2b5295cd7b3d80cba616af9
fpatalagoity <fpatalagoity@hotmail.com>
2017-04-04 22:23:06 -0300

    agrego el string duplicate para imprimir

--- FileSystem/src/functions/config.c
+++ FileSystem/src/functions/config.c
@@ -13,12 +13,12 @@
 
 void config_print(){
 	printf("El puerto del FS: %d\n", configFileSystem->puerto);
-
+	printf("Punto de montaje: %s\n", configFileSystem->punto_montaje);
 }
 void config_read(char* path){
 	t_config* config = config_create(path);
 	configFileSystem->puerto=config_get_int_value(config,"PUERTO");
-	configFileSystem->punto_montaje=config_get_string_value(config,"PUNTO_MONTAJE");
-	printf("Punto de montaje: %s\n", configFileSystem->punto_montaje);
+	configFileSystem->punto_montaje=string_duplicate(config_get_string_value(config,"PUNTO_MONTAJE"));
+
 	config_destroy(config);
 }
