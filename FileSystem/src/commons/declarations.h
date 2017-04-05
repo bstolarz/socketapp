e546eba99f287518ee4d53c6f66979993b5db2c2
fpatalagoity <fpatalagoity@hotmail.com>
2017-04-04 21:05:01 -0300

    lectura del archivo de configuracion del FS

---
+++ FileSystem/src/commons/declarations.h
@@ -0,0 +1,13 @@
+/*
+ * declarations.h
+ *
+ *  Created on: 4/4/2017
+ *      Author: utnso
+ */
+#include "structures.h"
+#ifndef COMMONS_DECLARATIONS_H_
+#define COMMONS_DECLARATIONS_H_
+
+
+t_fileSystem* configFileSystem;
+#endif /* COMMONS_DECLARATIONS_H_ */
