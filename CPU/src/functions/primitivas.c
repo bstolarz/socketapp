761e4c62f2cf71033865a683b0befd90064d498f
fpatalagoity <fpatalagoity@hotmail.com>
2017-04-22 12:12:16 -0300

    archivo de primitivas

---
+++ CPU/src/functions/primitivas.c
@@ -0,0 +1,30 @@
+/*
+ * primitivas.c
+ *
+ *  Created on: 20/4/2017
+ *      Author: utnso
+ */
+//	#include "sintax.h"
+#include "primitivas.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <parser/metadata_program.h>
+#include <parser/parser.h>
+#include <parser/metadata_program.h>
+#include "../commons/structures.h"
+#include "../commons/declarations.h"
+#include <commons/collections/dictionary.h>
+#include <commons/string.h>
+#include <commons/string.h>
+#include <commons/error.h>
+
+
+int initialSize=-4;
+int currentPage=0;
+t_puntero* AnSISOP_definirVariable (t_nombre_variable identificador_variable){
+	dictionary_put()
+}
+/*t_puntero* AnSISOP_obtenerPosicionVariable(t_nombre_variable identificador_variable){
+
+}*/
---
+++ CPU/src/functions/primitivas.h
@@ -0,0 +1,16 @@
+/*
+ * primitivas.h
+ *
+ *  Created on: 20/4/2017
+ *      Author: utnso
+ */
+
+#ifndef FUNCTIONS_PRIMITIVAS_H_
+#define FUNCTIONS_PRIMITIVAS_H_
+//#include "sintax.h"
+#include <parser/metadata_program.h>
+#include <parser/parser.h>
+#include <parser/metadata_program.h>
+
+
+#endif /* FUNCTIONS_PRIMITIVAS_H_ */
