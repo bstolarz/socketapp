a37166baa3367223a7265836247a9517a7e1b1ff
ignacio <ignacio_ferrari@hotmail.com>
2017-05-03 19:23:16 -0300

    CPU: volvi a poner la funcion d prueba que crea un pcb sin kernel. Primitivas: extraje todo en funciones para que se lea mas facil(en archivo stack).

--- CPU/src/functions/stack.c
+++ CPU/src/functions/stack.c
@@ -32,7 +32,65 @@ t_indiceDelStack* stack_context_current()
 	return stackExecContext;
 }
 
-void stack_context_pop()
+void stack_pop()
 {
 	list_remove_and_destroy_element(pcb->indiceDeStack, list_size(pcb->indiceDeStack) - 1, stack_context_destroy);
+}
+
+void stack_add_arg(t_nombre_variable identificador_variable)
+{
+	t_indiceDelStack* currentContext = stack_context_current();
+
+	int index = identificador_variable - '0';
+	currentContext->args[index] = puntero_to_position(pcb->stackPosition);
+
+	log_info(logCPU,"[definirVar] arg {%c} en (pag: %d, offset: %d y size: %d)\n",
+			identificador_variable,
+			currentContext->args[index].page,
+			currentContext->args[index].off,
+			currentContext->args[index].size);
+}
+
+void stack_add_var(t_nombre_variable identificador_variable)
+{
+	t_indiceDelStack* currentContext = stack_context_current();
+	t_position* stackPositionAsPosition = malloc(sizeof(t_position));
+	*stackPositionAsPosition = puntero_to_position(pcb->stackPosition);
+
+	dictionary_put(currentContext->vars, string_from_format("%c", identificador_variable), stackPositionAsPosition);
+
+	log_info(logCPU,"[definirVar] var local {%c} en (pag: %d, offset: %d y size: %d)\n",
+			identificador_variable, stackPositionAsPosition->page, stackPositionAsPosition->off, stackPositionAsPosition->size);
+}
+
+t_position* stack_get_arg(t_nombre_variable identificador_variable)
+{
+	t_indiceDelStack* currentContext = stack_context_current();
+
+	int index = identificador_variable - '0';
+	return &currentContext->args[index];
+}
+
+t_position* stack_get_var(t_nombre_variable identificador_variable)
+{
+	t_indiceDelStack* currentContext = stack_context_current();
+
+	char keyStr[2] = { identificador_variable, '\0' };
+	return dictionary_get(currentContext->vars, keyStr);
+}
+
+t_puntero position_to_puntero(t_position* memoryPos)
+{
+	return memoryPos->page * pageSize + memoryPos->off;
+}
+
+t_position puntero_to_position(t_puntero direccion_variable)
+{
+	t_position pos;
+	div_t values = div(direccion_variable, pageSize);
+	pos.page = values.quot;
+	pos.off = values.rem;
+	pos.size = sizeof(t_valor_variable);
+
+	return pos;
 }
--- Memory/src/other/test.h
+++ Memory/src/other/test.h
@@ -1,8 +1,6 @@
 #ifndef TEST_H_
 #define TEST_H_
 
-void test_program_init_end();
-void init_some_programs();
 void test_cpu_connection();
 
 #endif
