b02839fbfb36fa96bb8344ade06497ba504ce0d2
bstolarz <brenda.stolarz@gmail.com>
2017-05-27 23:20:07 -0300

    funcion crearArchivo() con set de data para pruebas

--- FileSystem/src/init.c
+++ FileSystem/src/init.c
@@ -37,6 +37,63 @@ void config_free1(){
 	free(configFileSystem);
 }
 
+
+void crearArchivos()
+{
+	// Funcion que crea Metada y bitmap. Creemos que esto despues sera proveido por la catedra
+
+	int cantBloques = 64;
+
+	FILE *fp = fopen((char*) strcat(configFileSystem->punto_montaje, "/Metadata/Metadata.bin"), "ab+");
+
+	fprintf(fp, "TAMANIO_BLOQUES=%i\n", cantBloques);
+	fprintf(fp, "CANTIDAD_BLOQUES=5192\n");
+	fprintf(fp, "MAGIC_NUMBER=SADICA\n");
+	fclose(fp);
+
+	/*** b i t  a r r a y ***/
+
+	char * bitArray = (char *) malloc(cantBloques);
+	int i;
+
+	FILE * fp2 = fopen((char*) strcat(configFileSystem->punto_montaje, "/Metadata/Metadata.bin"), "ab+");
+	for(i=0;i<cantBloques;i++)
+		if(i==40 || i==21 || i==82 || i==3)
+			bitArray[i] = 1;
+		else
+			bitArray[i] = 1;
+	size_t result;
+	fwrite(bitArray, cantBloques * sizeof(char *), result, fp2);
+
+	fclose(fp2);
+
+	/*** m e t a  d a t a  p o r  a r c h i v o ***/
+
+	FILE *fp3 = fopen((char*) strcat(configFileSystem->punto_montaje, "/Archivos/passwords/alumnosSIGA.bin"), "ab+");
+	fprintf(fp3, "TAMANIO=250\n");
+	fprintf(fp3, "BLOQUES=[40,21,82,3]");
+
+	fclose(fp3);
+
+	/*** b l o q u e s  d e  d a t o s ***/
+	FILE * fpBloque = fopen((char*) strcat(configFileSystem->punto_montaje, "/Archivos/passwords/40.bin"), "ab+");
+	fprintf(fpBloque, "Este es el bloque 40");
+	fclose(fpBloque);
+
+	fpBloque = fopen((char*) strcat(configFileSystem->punto_montaje, "/Archivos/passwords/21.bin"), "ab+");
+	fprintf(fpBloque, "Este es el bloque 21");
+	fclose(fpBloque);
+
+	fpBloque = fopen((char*) strcat(configFileSystem->punto_montaje, "/Archivos/passwords/82.bin"), "ab+");
+	fprintf(fpBloque, "Este es el bloque 82");
+	fclose(fpBloque);
+
+	fpBloque = fopen((char*) strcat(configFileSystem->punto_montaje, "/Archivos/passwords/3.bin"), "ab+");
+	fprintf(fpBloque, "Este es el bloque 3");
+	fclose(fpBloque);
+
+}
+
 void initSadica(){
 
 	//1. File System (Tiene tamanio bloques, cant bloques, magic number)
@@ -44,7 +101,7 @@ void initSadica(){
 	//3. Metadata (tamanio real del archivo en bytes, array con nro bloques en orden
 		// TAMANIO=250  BLOQUES=[40,21,82,3]
 
-
+	crearArchivos();
 	/***** Abro archivos ******/
 	config_read1(strcat(configFileSystem->punto_montaje, "/Metadata/Metadata.bin"));
 
