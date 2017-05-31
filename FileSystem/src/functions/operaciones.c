ccada96b3b8a680c5fe58c9f643029f0b0af84f5
TomasRS <tomas_rs@hotmail.com>
2017-05-30 23:13:16 -0300

    Empiezo guardarDatos de FS

--- FileSystem/src/functions/operaciones.c
+++ FileSystem/src/functions/operaciones.c
@@ -5,14 +5,15 @@
 #include <commons/config.h>
 #include "bitmap.h"
 #include "config.h"
+#include <math.h>
 #include "../commons/structures.h"
 #include "../commons/declarations.h"
 #include "auxiliares.h"
 
 //Implementacion de operaciones, por ahora son dummies
-int validar(char* path){
+int validar(char* path) {
 	int resultado = access(path, F_OK);
-	if(resultado == 0){
+	if (resultado == 0) {
 		log_info(logs, "Se encontro el archivo para el path: %s", path);
 		return 1;
 	}
@@ -22,55 +23,67 @@ int validar(char* path){
 	return -ENOENT;
 }
 
-int crear(char* path){
+int crear(char* path) {
 	int posBloqueLibre = encontrarUnBloqueLibre();
-	if (posBloqueLibre >= 0){
+	if (posBloqueLibre >= 0) {
 		ocuparBloqueLibre(posBloqueLibre);
 		crearArchivo(path, posBloqueLibre);
 		log_info(logs, "Se creo el archivo");
-	}
-	else{
+	} else {
 		log_info(logs, "No se encontro un bloque libre en el bitmap");
 		return -ENOENT;
 	}
 	return 0;
 }
 
-int borrar(char* path){
+int borrar(char* path) {
 	//Valido que exista el archivo
-	if(validar(path) == 1){
+	if (validar(path) == 1) {
 		t_metadata_archivo* archivo = malloc(sizeof(t_metadata_archivo));
-			read_fileMetadata(path, archivo);
+		read_fileMetadata(path, archivo);
 
-			list_iterate(archivo->bloques, (void*)liberarBloqueDelBitmap);
-			eliminarMetadataArchivo(path);
+		list_iterate(archivo->bloques, (void*) liberarBloqueDelBitmap);
+		eliminarMetadataArchivo(path);
 
-			list_destroy(archivo->bloques);
-			free(archivo);
-	}
-	else{
-		log_info(logs, "No se encontro el archivo, por ende no se lo puede borrar");
+		list_destroy(archivo->bloques);
+		free(archivo);
+	} else {
+		log_info(logs,
+				"No se encontro el archivo, por ende no se lo puede borrar");
 	}
 
 	return -ENOENT;
 }
 
-int obtenerDatos(char* path, off_t offset, size_t size){
-	if(validar(path) == 1){
+int obtenerDatos(char* path, off_t offset, size_t size) {
+	if (validar(path) == 1) {
 		//hago las cosas
-	}
-	else{
-		log_info(logs, "No se encontro el archivo, por ende no se le puede obtener datos");
+	} else {
+		log_info(logs,
+				"No se encontro el archivo, por ende no se le puede obtener datos");
 	}
 	return -ENOENT;
 }
 
-int guardarDatos(char* path, off_t offset, size_t size, void* buffer){
-	if(validar(path) == 1){
-		//hago las cosas
-	}
-	else{
-		log_info(logs, "No se encontro el archivo, por ende no se le puede guardar datos");
+int guardarDatos(char* path, off_t offset, size_t size, void* buffer) {
+	if (validar(path) == 1) {
+		t_metadata_archivo* archivo = malloc(sizeof(t_metadata_archivo));
+		read_fileMetadata(path, archivo);
+		int tamanioArchivo = archivo->tamanio;
+		//double cantBloques = ceil((double)(offset / tamanioArchivo));
+
+		//int ultimoBloque = getUltimoBloqueDeDatosDelArchivo(path);
+		//size_t bytesEnUltimoBloque = getBytesUltimoBloque(ultimoBloque);
+		//Si faltan bytes por llenar
+		//if (bytesEnUltimoBloque < configMetadata->tamanioBloques) {
+		//	size_t bytesALlenar = configMetadata->tamanioBloques - bytesEnUltimoBloque;
+		//}
+
+		list_destroy(archivo->bloques);
+		free(archivo);
+	} else {
+		log_info(logs,
+				"No se encontro el archivo, por ende no se le puede guardar datos");
 	}
 	return -ENOENT;
 }
