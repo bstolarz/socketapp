f04ed1c39306ebe16a483993d00faa9178806d80
TomasRS <tomas_rs@hotmail.com>
2017-06-08 19:14:25 -0300

    Vuelo Z-MiKernel y Z-MiConsola

--- Z-MiConsola/Debug/src/libSockets/client.d
+++
@@ -1,1 +0,0 @@
-src/libSockets/client.d: ../src/libSockets/client.c
--- Z-MiKernel/Debug/src/functions/config.d
+++
@@ -1,7 +0,0 @@
-src/functions/config.d: ../src/functions/config.c \
- ../src/functions/../commons/declarations.h \
- ../src/functions/../commons/structures.h
-
-../src/functions/../commons/declarations.h:
-
-../src/functions/../commons/structures.h:
